464214a4024d8539622bc4def2ee6004aeec5dde
veniarskyi <veniarskyi@gmail.com>
2023-07-10 14:36:22 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-fffff
+let num = 42; //number
+let firstName = 'Pasha' //string
+const isProgrammer = true //boolean
+
+/*Can Do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let _num = 12
+let num_ = 12
+let first_name = 'Elena' //не прийнято
+let myNum = 34 //прийнято
+let num42 = 10
+*/
+
+/*Restricred
+let 42num = '11'
+let my-num = 1
+let false 
+*/
+
+
+//firstName = 'Max'
+//isProgrammer = false //error because of const
+
+//alert(firstName)
+//console.log('Test:', num, isProgrammer)
+
+///console.log(num + 10)
+//console.log(num - 10)
+//console.log(num * 10)
+//console.log(num / 10)
+
+//let num2 = num + 10
+//console.log(num, num2)
+//num = num2 + 1
+//num2 = num2 + 1
+//console.log(num, num2)
+
+//let num3 = num + (10 * 2) / 5 - 2
+//console.log(num3)
+
+//const fullName = firstName + ' Nhik'
+//console.log(fullName)
