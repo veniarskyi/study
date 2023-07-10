ed6ca51be6a3cc0187c15dbc8be191ba00dce05d
veniarskyi <veniarskyi@gmail.com>
2023-07-10 18:03:20 +0300

    commit

--- index.js
+++ index.js
@@ -43,3 +43,41 @@ let false
 
 //const fullName = firstName + ' Nhik'
 //console.log(fullName)
+
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
+//console.log(resultElement.textContent)
+//resultElement.textContent = 42
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+minusBtn.onclick = function () {
+    action = "-"
+}
+
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result
+}
+
+submitBtn.onclick = function () {
+    if (action == '+') {
+    const sum = Number(input1.value) + Number(input2.value)
+    printResult(sum)
+    
+
+    } else if (action == '-') {
+        const sum = Number(input1.value) - Number(input2.value)
+       printResult(sum)
+    }
+}
